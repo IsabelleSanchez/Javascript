d4349cdd00e7ad25893d5eac2404b34154bfc3ef
Isabelle Sanchez <dasilvasanchezisabelle@gmail.com>
2025-09-02 10:51:56 -0300

    Versão comentada

--- nodejs/modulos/fs/app.js
+++ nodejs/modulos/fs/app.js
@@ -1,12 +1,18 @@
+// importando o módulo 'fs' para operações de sistema de arquivos
 const fs = require('fs');
 
+// Lendo um arquivo de forma assíncrona
 fs.readFile('example.txt', 'utf8', (err, data) => {
+    // Tratando erros
     if (err) {
+        // Se ocorrer um erro, exibe a mensagem de erro
         console.error('Erro ao ler arquivo:', err);
+        // retornando para evitar continuar a execução
         return; 
     }
-
+    // Se a leitura for bem-sucedida, exibe o conteúdo do arquivo
     console.log('Conteúdo do arquivo:', data);
 })
 
+// Mensagem para demonstrar a natureza assíncrona da operação de leitura de arquivo 
 console.log('Esta mensagem aparece primeiro.');
--- nodejs/modulos/os/app.js
+++ nodejs/modulos/os/app.js
@@ -1,2 +1,15 @@
+// importando o módulo 'os' para operações relacionadas ao sistema operacional
 const os = require('os');
+
+// Exibindo informações sobre o sistema operacional
+
+// exibindo arquitetura do sistema
+console.log('Arquiterura do sistema:', os.arch());
+// exibindo informações sobre a CPU
+console.log('Quantidade de CPUs:', os.cpus().length);
+// exibindo informações sobre a memória
+console.log('Memória total (bytes):', os.totalmem());
+// exibindo informações sobre a memória livre
+console.log('Memória livre (bytes):', os.freemem());
+// exibindo o sistema operacional
 console.log(os.platform());
